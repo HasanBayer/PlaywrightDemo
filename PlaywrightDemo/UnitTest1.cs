23167fd7d25feefc21ead8ce4b3ffc23cd185db6
Hasan BAYER <hasanbayer@gmail.com>
2024-12-09 05:25:30 -0500

    First TestCase-Login the page

--- PlaywrightDemo/UnitTest1.cs
+++ PlaywrightDemo/UnitTest1.cs
@@ -1,3 +1,7 @@
+using Microsoft.Playwright;
+using System.Net;
+using System.Text.RegularExpressions;
+
 namespace PlaywrightDemo
 {
     public class Tests
@@ -8,9 +12,25 @@ namespace PlaywrightDemo
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(
+                new BrowserTypeLaunchOptions { Headless=false, SlowMo=50, Timeout=80000} 
+            );
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync(url: "https://www.automationexercise.com/");            
+            await page.ClickAsync(".fa-lock");
+            playwright.Selectors.SetTestIdAttribute("data-qa");
+            await page.GetByTestId("login-email").FillAsync("abc123@hotmail.com");
+            await page.GetByTestId("login-password").FillAsync("123456789");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+
+
+
+
         }
     }
 }
