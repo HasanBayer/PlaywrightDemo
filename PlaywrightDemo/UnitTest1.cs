be4ef71a769198fa9a5ecd419ed477af3493d8f5
Hasan BAYER <hasanbayer@gmail.com>
2024-12-11 05:28:49 -0500

    Case3 - Multiple Browsers Login Test

--- PlaywrightDemo/UnitTest1.cs
+++ PlaywrightDemo/UnitTest1.cs
@@ -88,7 +88,82 @@ namespace PlaywrightDemo
             // Close the page
             await page.CloseAsync();
         }
+
+        [Test]
+        public async Task Test3()
+        {
+                // List of browsers to test
+                var browserTypes = new[] { "chromium", "firefox", "webkit" };
+
+                foreach (var browserType in browserTypes)
+                {
+                    // Create Playwright instance
+                    using var playwright = await Playwright.CreateAsync();
+
+                    // Launch the appropriate browser
+                    IBrowser browser = browserType switch
+                    {
+                        "chromium" => await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                        {
+                            Headless = false,
+                            SlowMo = 50,
+                            Timeout = 80000
+                        }),
+                        "firefox" => await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+                        {
+                            Headless = false,
+                            SlowMo = 50,
+                            Timeout = 80000
+                        }),
+                        "webkit" => await playwright.Webkit.LaunchAsync(new BrowserTypeLaunchOptions
+                        {
+                            Headless = false,
+                            SlowMo = 50,
+                            Timeout = 80000
+                        }),
+                        _ => throw new NotSupportedException($"Browser type {browserType} is not supported")
+                    };
+
+                    // Create a new browser context
+                    var context = await browser.NewContextAsync();
+
+                    // Create a new page
+                    var page = await context.NewPageAsync();
+
+                    // Navigate to the URL
+                    const string url = "https://www.automationexercise.com/";
+                    await page.GotoAsync(url);
+
+                    // Click on the login button
+                    await page.ClickAsync(".fa-lock");
+
+                    // Set the attribute for locating test elements
+                    playwright.Selectors.SetTestIdAttribute("data-qa");
+
+                    // Fill in the login form
+                    const string email = "abc123@hotmail.com";
+                    const string password = "123456789";
+                    await page.GetByTestId("login-email").FillAsync(email);
+                    await page.GetByTestId("login-password").FillAsync(password);
+
+                    // Click the login button
+                    await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+
+                    // Verify login success
+                    var loggedInUserLocator = page.Locator(".fa-user").First;
+                    await Assertions.Expect(loggedInUserLocator).ToBeVisibleAsync();
+
+                    // Ensure the "Logged in as" text is present
+                    var menuItemsLocator = page.Locator("ul > li");
+                    await Assertions.Expect(menuItemsLocator).ToContainTextAsync(new[] { "Logged in as" });
+
+                    // Close the page
+                    await page.CloseAsync();
+
+                    // Close the browser
+                    await browser.CloseAsync();
+                }
+            }
+        }
+
     }
-
-
-}
